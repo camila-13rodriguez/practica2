ee8f55af326e68b27f1eb62741fd9e498aad66d8
camila <jaquelinecamilarodriguez13@gmail.com>
2021-07-20 15:28:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function practica1(mensaje) {
+
+console.log(mensaje);
+
+ let a = 15;
+ a=25;
+const b = 10;
+
+b=1;
+
+console.log("hola mundo");
+}
+
+function practica2() {
+
+console.log("hola soy la funcion 2");
+
+const c=60;
+const d=20;
+console.log("el resultado de la suma de ", c ,d);
+console.log( sumar(c,d)   );
+
+
+}
+
+function sumar(n1,n2) {
+
+ let resultado=n1+n2;
+
+ return resultado;
+
+}
